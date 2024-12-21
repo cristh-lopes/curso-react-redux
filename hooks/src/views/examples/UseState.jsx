b963f4fb685e6feb059966fcea365e952487a635
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2024-12-21 14:42:19 -0300

    useState

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
+  const [num, setNum] = useState(0);
+  const [name, setName] = useState("");
   return (
     <div className="UseState">
       <PageTitle
         title="Hook UseState"
         subtitle="Estado em componentes funcionais!"
       />
+
+      <SectionTitle title="Exercício #01" />
+      <span className="text">{num} </span>
+      <div>
+        <button className="btn" onClick={() => setNum(num + 1)}>
+          +1
+        </button>
+        <button className="btn" onClick={() => setNum(num - 1)}>
+          -1
+        </button>
+        <button className="btn" onClick={() => setNum((cur) => cur + 1000)}>
+          +1000
+        </button>
+      </div>
+
+      <SectionTitle title="Exercício #01" />
+      <input
+        type="text"
+        className="input"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <span className="text">{name}</span>
     </div>
   );
 };
