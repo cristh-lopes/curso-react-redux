17b51e39bff92623d7f79610d4bce903829f3c58
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2024-12-22 18:33:12 -0300

    use reducer

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,33 +1,8 @@
 import React, { useReducer } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import { numberAdd2, login } from "../../store/actions";
+import { initialState, reducer } from "../../store";
 import SectionTitle from "../../components/layout/SectionTitle";
-
-const initialState = {
-  cart: [],
-  products: [],
-  user: null,
-  //foco...
-  number: 0,
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "number_add2":
-      return { ...state, number: state.number + 2 };
-    case "login":
-      return { ...state, user: action.payload };
-    case "number_mult7":
-      return { ...state, number: state.number * 7 };
-    case "number_div25":
-      return { ...state, number: state.number / 25 };
-    case "number_parseInt":
-      return { ...state, number: parseInt(state.number) };
-    case "number_addN":
-      return { ...state, number: state.number + action.payload };
-    default:
-      return state;
-  }
-}
 
 const UseReducer = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -41,16 +16,10 @@ const UseReducer = (props) => {
         <span className="text">{state.user || "Sem Usuário"}</span>
         <span className="text">{state.number}</span>
         <div>
-          <button
-            className="btn"
-            onClick={() => dispatch({ type: "login", payload: "Cristhian" })}
-          >
+          <button className="btn" onClick={() => login(dispatch, "Cristhian")}>
             Login
           </button>
-          <button
-            className="btn"
-            onClick={() => dispatch({ type: "number_add2" })}
-          >
+          <button className="btn" onClick={() => numberAdd2(dispatch)}>
             +2
           </button>
         </div>
@@ -77,9 +46,15 @@ const UseReducer = (props) => {
         </button>
         <button
           className="btn"
-          onClick={() => dispatch({ type: "number_addN", payload: 5 })}
+          onClick={() => dispatch({ type: "number_addN", payload: -9 })}
         >
-          +N
+          -9
+        </button>
+        <button
+          className="btn"
+          onClick={() => dispatch({ type: "numberAddN", payload: 11 })}
+        >
+          +11
         </button>
       </div>
     </div>
