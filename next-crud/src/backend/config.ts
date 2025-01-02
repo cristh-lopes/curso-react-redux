1ff0e838b89585e728f4308e39bd03ec8ab9b29d
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2025-01-02 14:09:56 -0300

    Repositorio de clientes

---
+++ next-crud/src/backend/config.ts
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+import "firebase/firebase";
+
+if (!firebase.apps.length)
+  firebase.initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  });
+
+export default firebase;
