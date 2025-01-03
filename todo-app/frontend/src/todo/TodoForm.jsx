2b8a4304e160bd1e5c5e17a4f483028f38d159d6
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2025-01-03 18:05:24 -0300

    Marcar como Concluído/Pendente

--- todo-app/frontend/src/todo/TodoForm.jsx
+++ todo-app/frontend/src/todo/TodoForm.jsx
@@ -4,24 +4,21 @@ import IconButton from "../template/IconButton";
 
 const TodoForm = (props) => (
   <div role="form" className="todoForm">
-    <Grid cols="12 9 10">
+    <div className="flex-end">
       <input
         id="description"
-        className="form-control"
+        className="form-control mr-1"
         placeholder="Adicione uma tarefa"
         onChange={props.handleChange}
         value={props.description}
-      ></input>
-    </Grid>
-
-    <Grid cols="12 3 2">
+      />
       {/* Mudamos o nome da propriedade style para btnStyle, pois o Lint reclamava e essa é a melhor maneira de contornar esse problema */}
       <IconButton
         btnStyle="primary"
         icon="plus"
         onClick={props.handleAdd}
       ></IconButton>
-    </Grid>
+    </div>
   </div>
 );
 
--- todo-app/frontend/src/todo/TodoList.jsx
+++ todo-app/frontend/src/todo/TodoList.jsx
@@ -6,13 +6,30 @@ const TodoList = (props) => {
     const list = props.list || [];
     return list.map((todo) => (
       <tr key={todo._id}>
-        <td>{todo.description}</td>
-        <td>
-          <IconButton
-            btnStyle="danger"
-            icon="trash-o"
-            onClick={() => props.handleRemove(todo)}
-          />
+        <td className={`col-sm-10 ${todo.done ? "markedAsDone" : ""}`}>
+          {todo.description}
+        </td>
+        <td className="col-sm-2">
+          <div className="flex-end">
+            <IconButton
+              btnStyle="success"
+              icon="check"
+              hide={todo.done}
+              onClick={() => props.handleMarkAsDone(todo)}
+            />
+            <IconButton
+              btnStyle="warning"
+              icon="undo"
+              hide={!todo.done}
+              onClick={() => props.handleMarkAsPending(todo)}
+            />
+            <IconButton
+              btnStyle="danger"
+              icon="trash-o"
+              hide={!todo.done}
+              onClick={() => props.handleRemove(todo)}
+            />
+          </div>
         </td>
       </tr>
     ));
@@ -21,8 +38,8 @@ const TodoList = (props) => {
     <table className="table">
       <thead>
         <tr>
-          <th>Descrição</th>
-          <th>Ações</th>
+          <th className="col-sm-10">Descrição</th>
+          <th className="col-sm-2 text-right pr-2">Ações</th>
         </tr>
       </thead>
       <tbody>{renderRows()}</tbody>
