0c647d015f2786263327e3f5fa90995050aebf41
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2025-01-03 14:54:27 -0300

    Eventos de formulário

--- todo-app/frontend/src/todo/Todo.jsx
+++ todo-app/frontend/src/todo/Todo.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 
+import PageHeader from "../template/PageHeader";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: "", list: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value });
+  }
+
+  handleAdd() {
+    console.log(this.state.description);
+  }
+
   render() {
     return (
       <div>
-        <h1>Todo</h1>
+        <PageHeader name="Tarefas" small="Cadastro" />
+        <TodoForm
+          description={this.state.description}
+          handleChange={this.handleChange}
+          handleAdd={this.handleAdd}
+        />
+        <TodoList />
       </div>
     );
   }
---
+++ todo-app/frontend/src/todo/TodoForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Grid from "../template/Grid";
+import IconButton from "../template/IconButton";
+
+const TodoForm = (props) => (
+  <div role="form" className="todoForm">
+    <Grid cols="12 9 10">
+      <input
+        id="description"
+        className="form-control"
+        placeholder="Adicione uma tarefa"
+        onChange={props.handleChange}
+        value={props.description}
+      ></input>
+    </Grid>
+
+    <Grid cols="12 3 2">
+      {/* Mudamos o nome da propriedade style para btnStyle, pois o Lint reclamava e essa é a melhor maneira de contornar esse problema */}
+      <IconButton
+        btnStyle="primary"
+        icon="plus"
+        onClick={props.handleAdd}
+      ></IconButton>
+    </Grid>
+  </div>
+);
+
+export default TodoForm;
